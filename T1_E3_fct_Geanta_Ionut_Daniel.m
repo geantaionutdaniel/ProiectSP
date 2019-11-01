6ad0b68af69cc4f02c640e46496c8e9e17925b46
geantaionutdaniel <57195384+geantaionutdaniel@users.noreply.github.com>
2019-11-01 16:01:03 +0200

    Delete T1_E3_fct_Geanta_Ionut_Daniel.m

--- T1_E3_fct_Geanta_Ionut_Daniel.m
+++
@@ -1,5 +0,0 @@
-function[med,v,mat]=ex3fct(a)
-med=mean(real(a)) %media aritmetica a partilor reale
-v=a.*a %elementele vectorului initial ridicate la patrat
-mat=a*a.' %vectorul initial inmultit cu transpusul sau
-end
